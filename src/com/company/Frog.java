a4ce8e52709ffd1e6d8bf8128cb899d3405c9f1a
Jason Bishop <jasonbishop11@gmail.com>
2019-10-07 12:22:53 -0400

    Initial Commit

---
+++ src/com/company/Frog.java
@@ -0,0 +1,105 @@
+package com.company;
+
+public class Frog {
+    private String name;
+    private int xPos;
+    private int yPos;
+
+    public Frog(String n,int x, int y)		// constructor
+    {
+        name = n;
+        xPos=x;
+        yPos=y;
+        System.out.println(name);
+    }
+
+    public Frog(){
+        name = "Steve";
+        xPos = 0;
+        yPos = 0;
+        System.out.println(name);
+    }
+
+    public void setPos(int x, int y)	// accessor methods
+    {
+        xPos = x;
+        yPos = y;
+    }
+    public int getXPos()
+    {
+        return xPos;
+    }
+    public int getYPos()
+    {
+        return yPos;
+    }
+    public void moveWest()		// change the pos of the frog
+    {
+        xPos = xPos-1;
+    }
+    public void moveEast()
+    {
+        xPos = xPos + 1;
+    }
+    public void moveNorth()
+    {
+        yPos=yPos+1;
+    }
+
+    public void jump(int dist){
+        int x = this.xPos+dist;
+        int y = this.yPos+dist;
+        this.setPos(x,y);
+    }
+
+    public void visit(Frog f){
+        int x = f.xPos;
+        int y = f.yPos;
+        this.setPos(x,y);
+    }
+
+    public void eatFly()
+    {
+        System.out.println("Yum");
+    }
+    public String toString()
+    {
+        String s = "Hi I am "+ name +" the frog and I live at "+ xPos + " " + yPos;
+        return s;
+    }
+
+
+    public static void main(String[] args) {
+        Frog f1 = new Frog("Freddy",3,4);	// create an instance of Frog
+        f1.eatFly();						// call Frog methods
+        f1.moveNorth();
+        System.out.println(f1);
+
+        Frog f2 = new Frog("Francine", -2,0);
+        f2.eatFly();
+        // TODO uncomment these to test new code.
+        f2.jump(2);
+        f1.visit(f2);
+        System.out.println(f1);
+        System.out.println(f2);
+
+        // make a new frog named Frank.  have him jump twice and then go visit Freddy.
+
+        Frog f3 = new Frog("Frank", 1, 5);
+        f3.jump(1);
+        f3.jump(3);
+        f3.visit(f1);
+        System.out.println(f3);
+    }
+
+}
+
+/*  Output
+Freddy
+Yum
+Hi I am Freddy the frog and I live at 3 5
+Francine
+Yum
+Hi I am Freddy the frog and I live at 0 2
+Hi I am Francine the frog and I live at 0 2
+*/
